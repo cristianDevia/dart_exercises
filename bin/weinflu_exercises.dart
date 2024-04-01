c7b327a7a31ec20dd45eb2fd9007b2121cf80e7e
Cristian Devia <c.devia8@gmail.com>
2024-03-31 20:23:55 -0500

    first commit

---
+++ bin/weinflu_exercises.dart
@@ -0,0 +1,5 @@
+import 'package:weinflu_exercises/weinflu_exercises.dart' as weinflu_exercises;
+
+void main(List<String> arguments) {
+  print('Hello world: ${weinflu_exercises.calculate()}!');
+}
---
+++ test/weinflu_exercises_test.dart
@@ -0,0 +1,8 @@
+import 'package:weinflu_exercises/weinflu_exercises.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
